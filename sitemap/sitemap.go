0b2595bc58afe8053bb15a7f86af4c3b25f640d7
iliut <iliuta_filip@yahoo.com>
2025-06-19 03:40:55 +0300

    -

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -1,11 +1,13 @@
 package sitemap
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"web-crawler/link_parser"
@@ -15,28 +17,96 @@ const xmlns = "http://sitemaps.org/schemas/sitemap/0.9"
 
 type loc struct {
 	Value string `xml:"loc"`
-}	
+}
 
 type urlset struct {
-	Urls []loc `xml:"url"`
+	Urls  []loc  `xml:"url"`
 	Xmlns string `xml:"xmlns,attr"`
 }
 
+type Job struct {
+	URL      string
+	MaxDepth int
+}
+
+func workers(id int, jobs <-chan Job, results chan<- []Job, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		links := get(job.URL)
+		if links != nil {
+			newJobs := make([]Job, 0, len(links))
+			for _, link := range links {
+				if job.MaxDepth > 0 {
+					newJobs = append(newJobs, Job{URL: link, MaxDepth: job.MaxDepth - 1})
+				} else {
+					newJobs = append(newJobs, Job{URL: link, MaxDepth: 0})
+				}
+			}
+			results <- newJobs
+		} else {
+			log.Printf("worker %d: no links found for %s", id, job.URL)
+			results <- nil
+		}
+	}
+}
+
 func GetLinks(url string, maxDepth int) []string {
-	
-	return bfs(url, maxDepth)
+
+	const numWorkers = 10
+
+	seen := make(map[string]struct{})
+	jobs := make(chan Job, 100)
+	results := make(chan []Job, 100)
+
+	var seenMu sync.Mutex
+	var wg sync.WaitGroup
+
+	for i := range numWorkers {
+		wg.Add(1)
+		go workers(i, jobs, results, &wg)
+	}
+
+	seen[url] = struct{}{}
+	pendingJobs := 1
+	jobs <- Job{URL: url, MaxDepth: maxDepth}
+
+	for pendingJobs > 0 {
+		newJobs := <-results
+		pendingJobs--
+
+		for _, job := range newJobs {
+			seenMu.Lock()
+			if _, ok := seen[job.URL]; !ok {
+				seen[job.URL] = struct{}{}
+				pendingJobs++
+				jobs <- job
+			}
+			seenMu.Unlock()
+		}
+	}
+
+	close(jobs)
+	wg.Wait()
+	close(results)
+
+	ret := make([]string, 0, len(seen))
+	for link := range seen {
+		ret = append(ret, link)
+	}
+	return ret
+	// return bfs(url, maxDepth)
 }
 
 type empty struct{}
 
 func bfs(urlStr string, maxDepth int) []string {
-	
+
 	seen := make(map[string]empty)
 	var q map[string]empty
 	nq := map[string]empty{
 		urlStr: {},
 	}
-	for range maxDepth+1 {
+	for range maxDepth + 1 {
 		q, nq = nq, make(map[string]empty)
 		if len(q) == 0 {
 			break
@@ -60,13 +130,15 @@ func bfs(urlStr string, maxDepth int) []string {
 	return ret
 }
 
-
 var client = &http.Client{
-	Timeout: 10 * time.Second,
+	Timeout: 5 * time.Second,
 }
 
 func get(urlStr string) []string {
-	req, err := http.NewRequest("GET", urlStr, nil)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", urlStr, nil)
 	if err != nil {
 		log.Printf("error creating request for %s: %v", urlStr, err)
 		return nil
@@ -125,7 +197,7 @@ func hrefs(body io.Reader, base string) []string {
 	return ret
 }
 
-func filter(links []string, keepFn func(string) bool ) []string {
+func filter(links []string, keepFn func(string) bool) []string {
 	var ret []string
 
 	for _, link := range links {
